9873899bf2f7c3f62a41262201bf8feed8e4a11c
hakuginn220 <hakuginn220@white-falls.com>
2016-11-15 14:08:54 +0900

    add vim-plug

--- .vimrc
+++ .vimrc
@@ -1,39 +1,8 @@
-"NeoBundle Scripts-----------------------------
-if &compatible
-  set nocompatible               " Be iMproved
-endif
-
-" Required:
-set runtimepath+=~/.vim/bundle/neobundle.vim/
-
-" Required:
-call neobundle#begin(expand('~/.vim/bundle'))
-
-" Let NeoBundle manage NeoBundle
-" Required:
-NeoBundleFetch 'Shougo/neobundle.vim'
-
-" Add or remove your Bundles here:
-NeoBundle 'Shougo/unite.vim'
-NeoBundle 'editorconfig/editorconfig-vim'
-NeoBundle 'cocopon/iceberg.vim'
-NeoBundle 'scrooloose/nerdtree'
-NeoBundle 'digitaltoad/vim-pug'
-NeoBundle 'statianzo/vim-jade'
-NeoBundle 'stephenway/postcss.vim'
-NeoBundle 'othree/yajs.vim'
-NeoBundle 'mattn/emmet-vim'
-
-" Required:
-call neobundle#end()
-
-" Required:
-filetype plugin indent on
-
-" If there are uninstalled bundles found on startup,
-" this will conveniently prompt you to install them.
-NeoBundleCheck
-"End NeoBundle Scripts-------------------------
+" vim-plug
+call plug#begin('~/.vim/plugged')
+Plug 'cocopon/iceberg.vim'
+Plug 'scrooloose/nerdtree'
+call plug#end()
 
 " システム設定
 set t_ut=
--- install.sh
+++ install.sh
@@ -1,5 +1,47 @@
 #!/bin/sh
-source ./bin/symlink.sh
-source ./bin/homebrew.sh
-source ./bin/anyenv.sh
-source ./bin/neobundle.sh
+
+# symlink
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# install homebrew
+if [ ! -x "`which brew`" ] ; then
+  exec /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  exec brew update
+  exec brew upgrade
+  exec brew install git
+  exec brew install openssl zsh tig tmux
+  exec brew install wget cloc tree
+  exec brew install go lua
+  exec brew install vim --with-lua
+  exec brew install heroku
+  exec $SHELL -l
+  exec brew cleanup
+  exec brew doctor
+fi
+
+# install anyenv
+if [ ! -d ~/.anyenv ] ; then
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+  exec $SHELL -l
+fi
+
+if [ ! -x "`which ndenv`" ] ; then
+  exec anyenv install ndenv
+  exec $SHELL -l
+fi
+
+if [ ! -x "`which rbenv`" ] ; then
+  exec anyenv install rbenv
+  exec $SHELL -l
+fi
+
+# install vim-plug
+if [ ! -d ~/.vim ] ; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
