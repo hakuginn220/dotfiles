e8853b54c16c4b1526ef3a8b6497e812c6c6e065
hakuginn220 <hakuginn220@white-falls.com>
2020-02-24 03:35:21 +0900

    update: add rustup

--- .bashrc
+++ .bashrc
@@ -1,1 +1,8 @@
+# node
 eval "$(nodenv init -)"
+
+# go
+export GOPATH=$HOME/go
+
+# rust
+export PATH="$HOME/.cargo/bin:$PATH"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,12 @@
 # fish setting
 set fish_greeting
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# node
 nodenv init - | source
+
+# go
+set -x -U GOPATH $HOME/go
+
+# rust
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
